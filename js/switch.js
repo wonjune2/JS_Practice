9aee41a792bc0c787d4e0ecc02db7e64261239e7
wonjune2 <zzzdnjswns@naver.com>
2024-06-26 04:21:31 +0900

    0626

---
+++ js/switch.js
@@ -0,0 +1,16 @@
+let a = 2 + 2;
+
+switch(a) {
+    case 3:
+        console.log("비교하려는 값보다 작습니다.");
+        break;
+    case 4:
+        console.log("비교하려는 값과 일치합니다.");
+        break;
+    case 5: 
+        console.log("비교하려는 값보다 큽니다.");
+        break;
+    default:
+        console.log("어떤 값인지 파악이 되지 않습니다.");
+}
+
