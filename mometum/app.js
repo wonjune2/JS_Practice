325e5d0bf50b9c9e09c12e07ca83ad45f77762db
wonjune2 <zzzdnjswns@naver.com>
2024-06-28 05:42:09 +0900

    objects

---
+++ mometum/app.js
@@ -0,0 +1,9 @@
+const player = {
+    name: "nico",
+    sayHello: function(otherPersonsName) {
+        console.log(`Hello ${otherPersonsName}`);
+    },
+};
+
+console.log(player.name);
+player.sayHello("wonJun");
