2d52cf61e0196c4f497f80c709a1221d1df3f723
wonjune2 <zzzdnjswns@naver.com>
2024-07-02 02:31:25 +0900

    event

--- mometum/app.js
+++ mometum/app.js
@@ -1,3 +1,17 @@
 const title = document.querySelector(".hello h1");
 
-console.log(title);
+function handleTitleClick() {
+    title.style.color = "blue";
+}
+
+function handleMouseEnter() {
+    title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+    title.innerText = "Mouse is gone!";
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
